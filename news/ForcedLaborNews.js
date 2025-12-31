7464703d626aa3d7d88eaae695a431d7ac3eb0ff
Daniel Ngo <danbngo@hotmail.com>
2025-12-31 00:15:54 -0500

    fixed forced labor news

--- news/ForcedLaborNews.js
+++ news/ForcedLaborNews.js
@@ -2,38 +2,48 @@ class ForcedLaborNews extends News {
     constructor(planet = new Planet()) {
         super(
             `${coloredName(planet)} implements brutal forced labor programs! Citizens are pressed into industrial work camps!`,
-            `${coloredName(planet)}'s forced labor camps are finally dismantled!`,
-            `Slave revolts force ${coloredName(planet)} to shut down labor camps!`,
+            `${coloredName(planet)}'s forced labor policies produce staggering economic gains despite their human cost!`,
+            `Worker revolts force ${coloredName(planet)} to shut down their forced labor program!`,
             ``,
             NT.FORCED_LABOR, planet
         )
 
         this.addPlanetEffect(
             {
+                industry: CL.HIGH,
+                population: CL.SLIGHTLY_LOW,
+                prestige: CL.LOW,
+                education: CL.SLIGHTLY_LOW,
+                culture: CL.LOW
+            },
+            {
                 industry: CL.VERY_HIGH,
-                population: CL.LOW,
+                economy: CL.SLIGHTLY_HIGH,
+                population: CL.SLIGHTLY_LOW,
                 prestige: CL.LOW,
+                education: CL.SLIGHTLY_LOW,
+                reserves: CL.VERY_HIGH,
+                taxes: CL.LOW,
+                culture: CL.LOW
             },
             {
-                industry: CL.SLIGHTLY_HIGH,
                 prestige: CL.LOW,
-            },
-            {
-                population: CL.VERY_LOW,
-                prestige: CL.VERY_LOW,
-                crime: CL.VERY_HIGH,
+                education: CL.SLIGHTLY_LOW,
+                reserves: CL.VERY_HIGH,
+                taxes: CL.LOW,
+                culture: CL.LOW
             }
         )
     }
 
     determineOutcome() {
-        this.rollOutcome(this.planet.c.security)
+        this.rollOutcome(this.planet.c.security*this.planet.c.army*this.planet.c.corruption/this.planet.c.education/this.planet.c.culture)
     }
 
     isValid() {
         const {planet: p} = this
         // More likely if industry is low (trying to industrialize)
-        const ratingsValid = p.c.industry < CL.LOW
+        const ratingsValid = p.c.industry < CL.LOW && p.c.corruption > CL.SLIGHTLY_HIGH
         return ratingsValid
     }
 }
