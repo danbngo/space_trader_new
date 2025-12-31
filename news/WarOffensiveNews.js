f676007b546f3f49d09b55014454f911834fa94a
Daniel Ngo <danbngo@hotmail.com>
2025-12-31 04:24:24 -0500

    fixed war offensive news

--- news/WarOffensiveNews.js
+++ news/WarOffensiveNews.js
@@ -1,34 +1,45 @@
 class WarOffensiveNews extends News {
     constructor(planet = new Planet(), targetPlanet = new Planet()) {
         super(
-            `${coloredName(planet)} launches a major naval offensive against ${coloredName(targetPlanet)}, relying on its generals!`,
+            `${coloredName(planet)} launches a major naval offensive against ${coloredName(targetPlanet)}, relying on its generals to turn the tide!`,
             `${coloredName(planet)}'s brilliant naval offensive has wreaked havoc on ${coloredName(targetPlanet)}!`,
-            '',
+            `${coloredName(planet)}'s naval incursion was deftly enveloped and destroyed in detail by ${coloredName(targetPlanet)}! Losses were catastrophic!`,
             `${coloredName(planet)}'s naval offensive against ${coloredName(targetPlanet)} is cancelled! Peace treaty signed!`,
             NT.WAR_OFFENSIVE, planet, targetPlanet
         )
 
         this.addPlanetEffect(
             {
-                military: CL.LOW,
+                military: CL.SLIGHTLY_LOW,
+                navy: CL.LOW
             },
             {
-                military: CL.SLIGHTLY_HIGH,
+                navy: CL.SLIGHTLY_LOW,
+                economy: CL.SLIGHTLY_LOW, //lose some control of shipping
             },
-            {},
-            {}
+            {
+                military: CL.SLIGHTLY_LOW,
+                navy: CL.VERY_LOW
+            },
         )
 
         this.addTargetPlanetEffect(
             {
-                military: CL.SLIGHTLY_LOW,
+                navy: CL.SLIGHTLY_LOW
             },
             {
-                military: CL.SLIGHTLY_LOW,
+                navy: CL.EXTREMELY_LOW,
+                economy: CL.SLIGHTLY_LOW
             },
-            {},
-            {}
+            {
+                navy: CL.SLIGHTLY_LOW
+            },
         )
+    }
+
+    determineOutcome() {
+        //simple as
+        return (this.planet.c.navy * this.planet.c.technology) / (this.targetPlanet.c.navy * this.targetPlanet.c.technology)
     }
 
     shouldCancel() {
@@ -39,10 +50,10 @@ class WarOffensiveNews extends News {
         const {planet: p, targetPlanet: tp} = this
         // Must be at war
         const relationshipValid = Civilization.areAtWar(p, tp)
-        // Our officers must be better than theirs
-        const advantage = p.c.education/tp.c.education >= CL.SLIGHTLY_HIGH
+        // our navy must be at least comparable
+        const ratingsValid = p.c.navy > CL.SLIGHTLY_LOW && p.c.navy > tp.c.navy
         // Can't have victory already
         const interferingEvent = News.hasNews(NT.WAR_OFFENSIVE, p, tp)
-        return relationshipValid && advantage && !interferingEvent
+        return relationshipValid && ratingsValid && !interferingEvent
     }
 }
