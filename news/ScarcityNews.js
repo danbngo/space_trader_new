bcd79d38b7c79e6a04bda0341948cc90cf6e828f
Daniel Ngo <danbngo@hotmail.com>
2025-12-31 02:09:54 -0500

    fixed scarcity news

--- news/ScarcityNews.js
+++ news/ScarcityNews.js
@@ -1,9 +1,9 @@
 class ScarcityNews extends News {
     constructor(planet = new Planet()) {
         super(
-            `${coloredName(planet)}'s overconsumption has led to famine and scarcity!`,
-            `${coloredName(planet)}'s great famine ends!`,
-            `${coloredName(planet)}'s famine spirals into catastrophic collapse!`,
+            `${coloredName(planet)}'s rampant overconsumption has led to famine and scarcity!`,
+            `${coloredName(planet)}'s famine abates thanks to its stockpiles and supplies from trading partners!`,
+            `${coloredName(planet)}'s famine sends the planet spiralling into social and economic free fall!`,
             ``,
             NT.SCARCITY, planet
         )
@@ -11,31 +11,45 @@ class ScarcityNews extends News {
         this.addPlanetEffect(
             {
                 population: CL.LOW,
-                reserves: CL.EXTREMELY_LOW,
-                cargoPriceMultipliers: new CountsMap(new Map([[CARGO_TYPES.WATER, CL.ASTRONOMICAL]]))
+                economy: CL.LOW,
+                industry: CL.SLIGHTLY_LOW,
+                wealth: CL.LOW,
+                reserves: CL.LOW,
+                cargoPriceMultipliers: new CountsMap(new Map([[CARGO_TYPES.FOOD, CL.ASTRONOMICAL], [CARGO_TYPES.WATER, CL.ASTRONOMICAL]]))
             },
             {
                 population: CL.SLIGHTLY_LOW,
+                economy: CL.SLIGHTLY_LOW,
+                industry: CL.SLIGHTLY_LOW,
+                wealth: CL.SLIGHTLY_LOW,
+                reserves: CL.SLIGHTLY_LOW
             },
             {
-                population: CL.NO_REGRESSION,
-                crime: CL.NO_REGRESSION,
-                prestige: CL.VERY_LOW
+                population: CL.VERY_LOW,
+                economy: CL.VERY_LOW,
+                industry: CL.LOW,
+                wealth: CL.LOW,
+                reserves: CL.EXTREMELY_LOW,
+                culture: CL.LOW,
+                security: CL.LOW,
+                crime: CL.HIGH,
+                corruption: CL.HIGH
             }
         )
     }
 
     determineOutcome() {
         const {planet: p} = this
-        // Scarcity ends unless economy/industry collapse further
-        this.rollOutcome(1 - (1 - p.c.economy) * (1 - p.c.industry) * 0.3)
+        //basically good planning and economic connection mitigate it
+        this.rollOutcome((this.planet.c.economy + this.planet.c.prestige + this.planet.c.reserves)/3
+            / this.planet.c.population / this.planet.c.industry, CL.SLIGHTLY_LOW)
     }
 
     isValid() {
         const {planet: p} = this
         //more likely if high pop and high industry
-        const ratingsValid = p.c.population > CL.HIGH || p.c.industry >= CL.HIGH
-        const interferingEvent = News.planetHasAnyNews(planet, [NT.SCARCITY, ...NT_ECONOMY_BOOSTING])
+        const ratingsValid = p.c.population > CL.VERY_HIGH || p.c.industry >= CL.VERY_HIGH
+        const interferingEvent = News.planetHasAnyNews(p, NT_ECONOMY_BOOSTING)
         return ratingsValid && !interferingEvent
     }
 }
