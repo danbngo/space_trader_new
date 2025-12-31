40ffecf7d3fc48413087fe702d12775b5e10875c
Daniel Ngo <danbngo@hotmail.com>
2025-12-31 02:02:22 -0500

    fixed sanctions news

--- news/SanctionsNews.js
+++ news/SanctionsNews.js
@@ -2,7 +2,7 @@ class SanctionsNews extends News {
     constructor(planet = new Planet(), targetPlanet = new Planet()) {
         super(
             `${coloredName(planet)} imposes economic sanctions on ${coloredName(targetPlanet)}, damaging both economies!`,
-            `${coloredName(planet)}'s sanctions on ${coloredName(targetPlanet)} have been lifted!`,
+            `${coloredName(planet)}'s sanctions succeed in disrupting ${coloredName(targetPlanet)}'s economy!`,
             `${coloredName(planet)}'s sanctions on ${coloredName(targetPlanet)} backfire, causing more domestic harm!`,
             `${coloredName(planet)} ends sanctions on ${coloredName(targetPlanet)} as relations normalize!`,
             NT.SANCTIONS, planet, targetPlanet
@@ -10,57 +10,55 @@ class SanctionsNews extends News {
 
         this.addPlanetEffect(
             {
+                reserves: CL.SLIGHTLY_LOW,
+                economy: CL.SLIGHTLY_LOW,
+                wealth: CL.SLIGHTLY_LOW
+            },
+            {
+                reserves: CL.SLIGHTLY_LOW,
+                wealth: CL.SLIGHTLY_LOW,
+                prestige: CL.SLIGHTLY_HIGH
+            },
+            {
                 reserves: CL.LOW,
                 economy: CL.LOW,
+                wealth: CL.LOW,
+                prestige: CL.SLIGHTLY_LOW
             },
-            {
-                economy: CL.SLIGHTLY_LOW,
-            },
-            {
-                economy: CL.VERY_LOW,
-                prestige: CL.LOW
-            },
-            {
-                reserves: CL.SLIGHTLY_HIGH,
-                economy: CL.SLIGHTLY_HIGH,
-            }
         )
 
         this.addTargetPlanetEffect(
             {
-                reserves: CL.VERY_LOW,
-                economy: CL.VERY_LOW,
+                reserves: CL.SLIGHTLY_LOW,
+                economy: CL.SLIGHTLY_LOW,
+                wealth: CL.SLIGHTLY_LOW
             },
             {
-                economy: CL.SLIGHTLY_LOW
+                reserves: CL.LOW,
+                economy: CL.LOW,
+                wealth: CL.LOW,
+                prestige: CL.LOW
             },
             {
-                economy: CL.SLIGHTLY_LOW
+                reserves: CL.SLIGHTLY_LOW,
+                wealth: CL.SLIGHTLY_LOW,
             },
-            {
-                reserves: CL.SLIGHTLY_HIGH,
-                economy: CL.SLIGHTLY_HIGH,
-            }
         )
     }
 
     shouldCancel() {
-        const {planet: p, targetPlanet: tp} = this
-        // Cancel if relationship improved
-        const rel = p.c.relationships.get(tp)
-        return rel === RELATIONSHIP_TYPES.NEUTRAL || rel === RELATIONSHIP_TYPES.ALLY
+        return Civilization.areAlliesOrNeutral(this.planet, this.targetPlanet)
     }
 
     determineOutcome() {
-        const {planet: p, targetPlanet: tp} = this
-        // Sanctions succeed unless sanctioner's economy weakens too much
-        this.rollOutcome(p.c.economy * 0.7 + 0.3)
+        this.rollOutcome(this.planet.c.economy*this.planet.c.wealth*this.planet.c.prestige
+            /this.targetPlanet.c.economy/this.targetPlanet.c.economy/this.targetPlanet.c.prestige)
     }
 
     isValid() {
         const {planet: p, targetPlanet: tp} = this
-        //we need a strong economy to pull it off
-        const ratingsValid = p.c.economy > CL.HIGH && p.c.wealth >= CL.HIGH
+        //we need a strong economy and prestige to pull it off
+        const ratingsValid = p.c.economy > CL.SLIGHTLY_HIGH && p.c.wealth > CL.SLIGHTLY_HIGH && p.c.prestige > CL.MEDIUM
         const relationshipsValid = Civilization.areTenseOrAtWar(p, tp)
         //blocked if already at war or other hostile actions
         const interferingEvent = News.hasAnyNewsBidirectional(p, tp, NT_COOPERATIVE)
