31365cfa82ff769fe01bb27afe1dcd4517e2767e
Daniel Ngo <danbngo@hotmail.com>
2025-12-31 04:46:03 -0500

    war subjugation news fixed

--- news/WarSubjugationNews.js
+++ news/WarSubjugationNews.js
@@ -3,7 +3,7 @@ class WarSubjugationNews extends News {
         super(
             `${coloredName(planet)} lands its ships and occupies ${coloredName(targetPlanet)}! Its armies raise the flag of ${coloredName(planet)} over the conquered world.`,
             `${coloredName(targetPlanet)} regains independence from ${coloredName(planet)}, bringing the occupation to an end!`,
-            `${coloredName(targetPlanet)} mounts fierce resistance and repels ${coloredName(planet)}'s occupation forces!`,
+            ``,
             `Peace treaty forces ${coloredName(planet)} to abandon occupation of ${coloredName(targetPlanet)}!`,
             NT.SUBJUGATION, planet, targetPlanet
         )
@@ -11,21 +11,23 @@ class WarSubjugationNews extends News {
         this.addPlanetEffect(
             {
                 newRelationship: RELATIONSHIP_TYPES.SOVEREIGN,
-                territory: CL.VERY_HIGH,
-                prestige: CL.VERY_HIGH
+                territory: CL.EXTREMELY_HIGH,
+                prestige: CL.EXTREMELY_HIGH,
+                economy: CL.HIGH,
+                industry: CL.SLIGHTLY_HIGH,
+                wealth: CL.HIGH,
+                inflation: CL.LOW,
+                taxes: CL.LOW, //tax them instead
+                army: CL.SLIGHTLY_LOW, //requires some troops to keep them in line
+                security: CL.LOW
             },
             {
                 newRelationship: RELATIONSHIP_TYPES.NEUTRAL,
-                territory: CL.SLIGHTLY_HIGH,
-                prestige: CL.SLIGHTLY_HIGH
+                prestige: CL.SLIGHTLY_HIGH,
             },
+            {}, //it cant fail due to timespan issue
             {
-                military: CL.LOW,
-                prestige: CL.LOW
-            },
-            {
-                territory: CL.SLIGHTLY_HIGH,
-                prestige: CL.SLIGHTLY_HIGH
+                prestige: CL.SLIGHTLY_HIGH,
             }
         )
 
@@ -33,21 +35,25 @@ class WarSubjugationNews extends News {
             {
                 governmentType: GT.PUPPET_STATE,
                 newRelationship: RELATIONSHIP_TYPES.SUBJECT,
-                military: CL.EXTREMELY_LOW,
+                army: CL.EXTREMELY_LOW,
+                navy: CL.EXTREMELY_LOW,
                 prestige: CL.VERY_LOW,
+                economy: CL.LOW, //boot goes on neck
+                industry: CL.SLIGHTLY_LOW,
+                wealth: CL.LOW,
+                inflation: CL.HIGH,
+                taxes: CL.HIGH, 
                 relationsReset: true,
                 cargoPriceMultipliers: new CountsMap(new Map([[CARGO_TYPES.ANTIMATTER, CL.EXTREMELY_LOW]])),
                 forcePeace: true
             },
             {
                 newRelationship: RELATIONSHIP_TYPES.NEUTRAL,
-                prestige: CL.SLIGHTLY_HIGH
+                prestige: CL.SLIGHTLY_LOW
             },
+            {},
             {
-                prestige: CL.HIGH,
-            },
-            {
-                prestige: CL.SLIGHTLY_HIGH
+                prestige: CL.SLIGHTLY_LOW
             }
         )
     }
@@ -57,16 +63,13 @@ class WarSubjugationNews extends News {
     }
 
     determineOutcome() {
-        const {planet: p, targetPlanet: tp} = this
-        // Resistance probability based on target's military strength
-        const successProbability = 1 - (tp.c.military / p.c.military) * CL.EXTREMELY_LOW
-        this.rollOutcome(successProbability)
+        this.rollOutcome((this.planet.c.military + this.planet.c.army)/2/(this.targetPlanet.c.military), CL.HIGH)
     }
 
     isValid() {
         const {planet: p, targetPlanet: tp} = this
         //our army must be both large and  significantly better than theirs in every way
-        const ratingsValid = (p.c.army/tp.c.army > CL.HIGH) && (p.c.navy/tp.c.navy > CL.HIGH)
+        const ratingsValid = p.c.army > CL.HIGH && (p.c.army/tp.c.army > CL.HIGH) && (p.c.navy/tp.c.navy > CL.HIGH)
         //planet must be at war with the target
         const relationshipValid = Civilization.areAtWar(p, tp)
         const interferingEvent = News.hasAnyNewsBidirectional(p, tp, NT_COOPERATIVE)
