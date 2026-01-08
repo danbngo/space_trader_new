a89a37da79981936263503a09307a59ec699ae47
Daniel Ngo <danbngo@hotmail.com>
2026-01-07 21:56:17 -0500

    added caravansery

---
+++ menus/caravanseryMenu.js
@@ -0,0 +1,18 @@
+/**
+ * Displays the caravansery menu where players can take escort missions.
+ * @param {Caravansery} caravansery - The caravansery building.
+ */
+function showCaravanseryMenu(caravansery) {
+    const planet = caravansery.planet
+    
+    let msg = `Welcome to the ${coloredName(planet)} Caravansery.<br/><br/>`
+    msg += `This establishment arranges escort missions for traders and travelers.<br/>`
+    msg += `<br/>`
+    msg += colorSpan(`(Escort missions coming soon!)`, COLORS.Gray)
+    
+    const buttons = [
+        ['Back', () => showPlanetMenu(planet)]
+    ]
+    
+    showModal('Caravansery', msg, buttons)
+}
